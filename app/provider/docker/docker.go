dfe4aa10d5e17f33d2db64b3b9a83e43137b4bb2
rantanevich <r.antanevich@gmail.com>
2025-03-15 19:03:06 +0300

    feat: support SSH connection to Docker Host

--- app/provider/docker/docker.go
+++ app/provider/docker/docker.go
@@ -3,12 +3,15 @@ package docker
 import (
 	"context"
 	"errors"
+	"fmt"
 	"io"
 	"log/slog"
+	"net/http"
 	"sync"
 	"time"
 
 	"github.com/avast/retry-go/v4"
+	"github.com/docker/cli/cli/connhelper"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/events"
 	"github.com/docker/docker/api/types/filters"
@@ -23,8 +26,9 @@ const DockerAPIVersion = "1.24"
 var _ provider.Provider = (*Provider)(nil)
 
 type Provider struct {
-	Endpoint string
-	Watch    bool
+	Endpoint          string
+	Watch             bool
+	HTTPClientTimeout time.Duration
 }
 
 func (p *Provider) SetDefaults() {
@@ -51,6 +55,7 @@ func (p *Provider) Provide(ctx context.Context, wg *sync.WaitGroup, updateCh cha
 
 			containers, err := dockerClient.ContainerList(ctx, container.ListOptions{})
 			if err != nil {
+				fmt.Printf("err: %v\n", err)
 				return err
 			}
 
@@ -112,6 +117,10 @@ func (p *Provider) Provide(ctx context.Context, wg *sync.WaitGroup, updateCh cha
 			retry.UntilSucceeded(),
 			retry.Delay(5*time.Second),
 			retry.MaxDelay(60*time.Second),
+			retry.Context(ctx),
+			retry.OnRetry(func(attempt uint, err error) {
+				log.Error("provider error, retrying", slog.String("error", err.Error()))
+			}),
 		)
 	}()
 
@@ -119,9 +128,9 @@ func (p *Provider) Provide(ctx context.Context, wg *sync.WaitGroup, updateCh cha
 }
 
 func (p *Provider) createClient() (*client.Client, error) {
-	opts := []client.Opt{
-		client.WithHost(p.Endpoint),
-		client.WithVersion(DockerAPIVersion),
+	opts, err := p.getClientOpts()
+	if err != nil {
+		return nil, err
 	}
 
 	dockerClient, err := client.NewClientWithOpts(opts...)
@@ -129,4 +138,36 @@ func (p *Provider) createClient() (*client.Client, error) {
 		return nil, err
 	}
 	return dockerClient, nil
+}
+
+func (p *Provider) getClientOpts() ([]client.Opt, error) {
+	helper, err := connhelper.GetConnectionHelper(p.Endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	opts := []client.Opt{
+		client.WithVersion(DockerAPIVersion),
+	}
+
+	if helper != nil {
+		// https://github.com/docker/cli/blob/ebca1413117a3fcb81c89d6be226dcec74e5289f/cli/context/docker/load.go#L112-L123
+		httpClient := &http.Client{
+			Transport: &http.Transport{
+				DialContext: helper.Dialer,
+			},
+		}
+
+		opts = append(
+			opts,
+			client.WithHTTPClient(httpClient),
+			client.WithTimeout(time.Duration(p.HTTPClientTimeout)),
+			client.WithHost(helper.Host), // To avoid 400 Bad Request: malformed Host header daemon error
+			client.WithDialContext(helper.Dialer),
+		)
+		return opts, nil
+	}
+
+	opts = append(opts, client.WithHost(p.Endpoint))
+	return opts, nil
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.1
 
 require (
 	github.com/avast/retry-go/v4 v4.6.1
+	github.com/docker/cli v28.0.1+incompatible
 	github.com/docker/docker v28.0.1+incompatible
 	github.com/fsnotify/fsnotify v1.8.0
 	github.com/traefik/paerser v0.2.2
@@ -26,6 +27,7 @@ require (
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.60.0 // indirect
 	go.opentelemetry.io/otel v1.35.0 // indirect
